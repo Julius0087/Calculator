8bf693b4d668f8ba412d2038735713f9dd99d2cf
Julius <123553495+Julius0087@users.noreply.github.com>
2023-06-01 16:04:08 +0200

    Update version
    - add event listeners for calculator buttons
    - add display population with selected buttons
    - add operator mutual exclusivity - only one can be selected

--- script.js
+++ script.js
@@ -2,6 +2,52 @@ let firstNumber;
 let operator;
 let secondNumber;
 
+let displayValue;
+const operatorArr = ["+", "-", "*", "/"];
+
+const display = document.querySelector(".display");
+
+const digit = document.querySelectorAll(".digit");
+for (i of digit) {
+  i.addEventListener("click", e => {
+    /* console.log(e.target); */
+    // populate the display
+    populateDisplay(e.target.textContent);
+  })
+}
+
+const operatorDiv = document.querySelectorAll(".operator");
+for (i of operatorDiv) {
+  i.addEventListener("click", e => {
+    // select the operator
+    operator = e.target.textContent;
+    if (operatorCheck()) updateOperator(operator); // contains any other operator -> update the operator
+    else {
+      populateDisplay(operator);
+    }
+  })
+}
+
+function operatorCheck() {
+  if (displayValue.includes("+") ||
+  displayValue.includes("-") ||
+  displayValue.includes("/") ||
+  displayValue.includes("*")) {
+    return true;
+  }
+}
+
+function updateOperator(operator) {
+  displayValue = displayValue.slice(0, -1);
+  displayValue += operator;
+  display.textContent = displayValue;
+}
+
+function populateDisplay(context) {
+  display.textContent += context;
+  displayValue = display.textContent;
+}
+
 function opearate(operator, x, y) {
   if (operator === "+") return add(x, y)
 }
@@ -22,5 +68,3 @@ function divide(x, y) {
   if (y === 0) return "Cannot divide by 0"
   return x / y;
 }
-
-console.log(opearate("+", 20, 20));
